c297c4ea23f443395ed9cc2354f56c6e2d2d665f
Ido-Studies <ido.israeli.studies@gmail.com>
2023-05-28 22:38:57 +0300

    Designed the home activity of the app. Buttons lead nowhere, and they display a toast upon being clicked.

--- app/src/main/java/com/idoisraeli/mobileminigames/MainActivity.java
+++ app/src/main/java/com/idoisraeli/mobileminigames/MainActivity.java
@@ -3,6 +3,9 @@ package com.idoisraeli.mobileminigames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello From First Actual Commit");
+
+        Button[] deadEndButtons = {
+                (Button) findViewById(R.id.btnLogin),
+                (Button) findViewById(R.id.btnLeaderboards),
+                (Button) findViewById(R.id.btnGame1),
+                (Button) findViewById(R.id.btnGame2),
+                (Button) findViewById(R.id.btnGame3),
+        };
+
+        View.OnClickListener partNotReady = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "This part of the App isn't ready yet.", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        for (Button btn:
+                deadEndButtons) {
+            btn.setOnClickListener(partNotReady);
+        }
     }
 }
