8d105d321355182c41548b7a75a58d6905962696
Ido-Studies <ido.israeli.studies@gmail.com>
2023-07-30 20:31:54 +0300

    Remade registration input validation to have it return a boolean.

--- app/src/main/java/com/idoisraeli/mobileminigames/RegisterActivity.java
+++ app/src/main/java/com/idoisraeli/mobileminigames/RegisterActivity.java
@@ -50,70 +50,11 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                registrationInputs.forEach((key, value) -> {
-                    if(TextUtils.isEmpty(value.getText().toString())) {
-                        Toast.makeText(RegisterActivity.this,
-                                "Please enter your " + key,
-                                Toast.LENGTH_SHORT).show();
-                        value.setError(key + " is a required field");
-                        value.requestFocus();
-                    }
-                    else {
-                        switch(key) {
-                            case "Email":
-                                if(!Patterns.EMAIL_ADDRESS.matcher(value.getText().toString()).matches()) {
-                                    Toast.makeText(RegisterActivity.this,
-                                            "Please re-enter your Email",
-                                            Toast.LENGTH_SHORT).show();
-                                    value.setError("Invalid Email Address");
-                                    value.requestFocus();
-                                }
-                                break;
-
-                            case "Age":
-                                boolean isInvalidAge = false;
-                                try {
-                                    if(Integer.parseInt(value.getText().toString()) <= 0)
-                                        isInvalidAge = true;
-                                    else
-                                        break;
-                                } catch (NumberFormatException nfe) {
-                                    isInvalidAge = true;
-                                }
-                                if(isInvalidAge) {
-                                    Toast.makeText(RegisterActivity.this,
-                                            "Please re-enter your age",
-                                            Toast.LENGTH_SHORT).show();
-                                    value.setError("Invalid age");
-                                    value.requestFocus();
-                                }
-                                break;
-
-                            case "Phone Number":
-//                                Toast.makeText(RegisterActivity.this,
-//                                        PhoneNumberUtils.formatNumberToE164(value.getText().toString(), "IL"),
-//                                        Toast.LENGTH_SHORT).show();
-                                if(value.getText().toString().length() != 10) {
-                                    Toast.makeText(RegisterActivity.this,
-                                            "Please re-enter your Phone Number",
-                                            Toast.LENGTH_SHORT).show();
-                                    value.setError("Invalid Phone Number");
-                                    value.requestFocus();
-                                }
-                                break;
-
-                            case "Password":
-                                if(value.getText().toString().length() < 8) {
-                                    Toast.makeText(RegisterActivity.this,
-                                            "Please re-enter your Password",
-                                            Toast.LENGTH_SHORT).show();
-                                    value.setError("Password must be 8 characters or more");
-                                    value.requestFocus();
-                                }
-                                break;
-                        }
-                    }
-                });
+                if(validateRegistrationInputs(registrationInputs)) {
+                    Toast.makeText(RegisterActivity.this,
+                            "Passed all the tests",
+                            Toast.LENGTH_SHORT).show();
+                }
 
 //                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
             }
@@ -128,4 +69,84 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
     }
+
+    public boolean validateRegistrationInputs(Map<String, EditText> registrationInputs) {
+        boolean areAllValid = true;
+
+        for (String key:
+             registrationInputs.keySet()) {
+            EditText value = registrationInputs.get(key);
+
+            if(TextUtils.isEmpty(value.getText().toString())) {
+                Toast.makeText(RegisterActivity.this,
+                        "Please enter your " + key,
+                        Toast.LENGTH_SHORT).show();
+                value.setError(key + " is a required field");
+                value.requestFocus();
+                areAllValid = false;
+            }
+            else {
+                switch(key) {
+                    case "Email":
+                        if(!Patterns.EMAIL_ADDRESS.matcher(value.getText().toString()).matches()) {
+                            Toast.makeText(RegisterActivity.this,
+                                    "Please re-enter your Email",
+                                    Toast.LENGTH_SHORT).show();
+                            value.setError("Invalid Email Address");
+                            value.requestFocus();
+                            areAllValid = false;
+                        }
+                        break;
+
+                    case "Age":
+                        boolean isInvalidAge = false;
+                        try {
+                            if(Integer.parseInt(value.getText().toString()) <= 0)
+                                isInvalidAge = true;
+                            else
+                                break;
+                        } catch (NumberFormatException nfe) {
+                            isInvalidAge = true;
+                        }
+                        if(isInvalidAge) {
+                            Toast.makeText(RegisterActivity.this,
+                                    "Please re-enter your age",
+                                    Toast.LENGTH_SHORT).show();
+                            value.setError("Invalid age");
+                            value.requestFocus();
+                            areAllValid = false;
+                        }
+                        break;
+
+                    case "Phone Number":
+//                                Toast.makeText(RegisterActivity.this,
+//                                        PhoneNumberUtils.formatNumberToE164(value.getText().toString(), "IL"),
+//                                        Toast.LENGTH_SHORT).show();
+                        if(value.getText().toString().length() != 10) {
+                            Toast.makeText(RegisterActivity.this,
+                                    "Please re-enter your Phone Number",
+                                    Toast.LENGTH_SHORT).show();
+                            value.setError("Invalid Phone Number");
+                            value.requestFocus();
+                            areAllValid = false;
+                        }
+                        break;
+
+                    case "Password":
+                        if(value.getText().toString().length() < 8) {
+                            Toast.makeText(RegisterActivity.this,
+                                    "Please re-enter your Password",
+                                    Toast.LENGTH_SHORT).show();
+                            value.setError("Password must be 8 characters or more");
+                            value.requestFocus();
+                            areAllValid = false;
+                        }
+                        break;
+                }
+            }
+        }
+
+        return areAllValid;
+    }
+
 }
