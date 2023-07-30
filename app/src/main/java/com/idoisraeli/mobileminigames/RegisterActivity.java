e36ce4a899229d63870b2b1618beb31653f82520
Ido-Studies <ido.israeli.studies@gmail.com>
2023-07-30 15:47:44 +0300

    Created and styled RegisterActivity.

--- app/src/main/java/com/idoisraeli/mobileminigames/RegisterActivity.java
+++ app/src/main/java/com/idoisraeli/mobileminigames/RegisterActivity.java
@@ -8,35 +8,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         Button goBackBtn = (Button) findViewById(R.id.goBackButton);
         goBackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
             }
         });
 
-        Button loginBtn = (Button) findViewById(R.id.loginButton);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        Button registerBtn = (Button) findViewById(R.id.registerButton);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
 
-        TextView registerLink = (TextView) findViewById(R.id.registerLink);
-        registerLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-            }
-        });
     }
 }
